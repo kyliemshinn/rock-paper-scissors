3825ffb7df17a138a34ae0029f24d33b47533520
Kylie Shinn <kyliemshinn@gmail.com>
2022-06-14 16:30:21 -0700

    add initial logic for the user guess conditional statement

--- main.js
+++ main.js
@@ -9,8 +9,16 @@ let ties = 0;
 
 //loop through number of times to play
 for (let i = 0; i < 5; i++) {
+    //create the computer guess at random
+    const computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    //create the user guess
     const userGuess = prompt("Enter rock, paper, or scissors to start playing");
     //send each guess to be lowercase
     userGuess = userGuess.toLowerCase();
+
+    if(userGuess === "rock" || userGuess === "paper" || userGuess === "scissors") {
+        alert("the computer chose" + computerGuess);
+        
+    }
 }
 
